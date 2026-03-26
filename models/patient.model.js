5d4b881117078db635304f5d071015b0f3598112
ayushsahu <ayushsahu97810@gmail.com>
2026-03-26 19:18:34 +0530

    usermodel

--- models/patient.model.js
+++ models/patient.model.js
@@ -1,11 +1,21 @@
+// importing mongoose to create model and connect with MongoDB
 const mongoose = require('mongoose');
-// this defines how doctor data will be stored in MongoDB
+
+// creating Patient model (this decides how patient data will look in database)
 module.exports = mongoose.model('Patient', {
+
+  // name of the patient
   name: String,
+
+  // age of the patient
   age: Number,
+
+  // disease or problem patient has
   disease: String,
-  // this links patient to a specific user (who created it)
-  // ObjectId is used to store reference of another collection
-  // ref: 'User' means it is connected to User model
+
+  // this field stores the id of the user who created this patient
+  // ObjectId means we are storing id (not normal string)
+  // ref: 'User' connects this field to User collection
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+
 });
--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,16 @@
+// importing mongoose to define schema and model
 const mongoose = require('mongoose');
 
+// creating User model (this defines how user data is stored in database)
 module.exports = mongoose.model('User', {
+
+  // name of the user
   name: String,
+
+  // email of the user (must be unique for each user)
   email: { type: String, unique: true },
+
+  // password of the user (will be stored in hashed form)
   password: String
+
 });

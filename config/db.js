31756c42ce1b088fe282d9825f210c1bdb284015
ayushsahu <ayushsahu97810@gmail.com>
2026-03-26 18:36:43 +0530

    first

--- config/db.js
+++ config/db.js
@@ -1,13 +1,23 @@
+// importing mongoose to connect with MongoDB
 const mongoose = require('mongoose');
+
+// this is used to read variables from .env file
 require('dotenv').config();
 
+// function to connect database
 const connectDB = async () => {
   try {
+    // connecting to MongoDB using URL stored in .env file
+    // (MONGO_URI contains our database connection string)
     await mongoose.connect(process.env.MONGO_URI);
+
+    // if connection is successful
     console.log("MongoDB connected");
   } catch (err) {
+    // if any error comes while connecting, print it
     console.log(err);
   }
 };
 
+// exporting this function so we can use it in server.js
 module.exports = connectDB;
